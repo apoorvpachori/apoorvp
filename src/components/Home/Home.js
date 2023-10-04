7ca307fbdb616f5a7faadf8ae9db95b38efc19cf
apoorvpachori <apoorv.pachori21@gmail.com>
2023-10-04 12:57:04 -0700

    homepage styling and animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,38 @@
+import { Link } from 'react-router-dom';
+import './Home.scss'
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+
 const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = [' ', 'A', 'p', 'o', 'o', 'r', 'v', ' ', 'P', 'a', 'c', 'h', 'o', 'r', 'i']
+
+    useEffect(() => {
+
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }
+
+
+    )
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi</h1>
+                <h1>Hi, <br /> I'm
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                    <br />
+                    Software Engineer
+                </h1>
+                <h2>Fullstack and Cloud Developer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+
             </div>
         </div>
     )
