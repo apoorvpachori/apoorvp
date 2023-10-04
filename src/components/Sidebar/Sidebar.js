50e4f080dd44974abf7b621fffe2375d4ef33ed1
apoorvpachori <apoorv.pachori21@gmail.com>
2023-10-04 11:34:53 -0700

    sidebar link and styling complete

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,7 +3,8 @@ import './sidebar.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSub from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Sidebar = () => {
     return (
         <div className='nav-bar'>
@@ -15,7 +16,25 @@ const Sidebar = () => {
                 <NavLink exact="true" activeclassname="active" to="/">
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-long" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/apoorvpachori/">
+                        <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/apoorvpachori/">
+                        <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
